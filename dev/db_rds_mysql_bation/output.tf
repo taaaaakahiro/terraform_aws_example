5f5b9c08e9118142205bd853e6277ec7b7e9af18
takahiro <takapi854@gmail.com>
2023-02-07 21:45:22 +0900

    update: dir name, output rds endpoint

--- dev/db_rds_mysql_bation/output.tf
+++ dev/db_rds_mysql_bation/output.tf
@@ -3,6 +3,10 @@ output "db_password" {
     value = module.rds.rds_password
 }
 
+output "db_instace_endpoint" {
+    value = module.rds.db_instace_endpoint
+}
+
 // iam
 output "iam_instance_profile_name"{
     value = module.iam.iam_instance_profile_name
--- dev/db_rds_mysql_bation/variable.tf
+++ dev/db_rds_mysql_bation/variable.tf
@@ -1,9 +1,9 @@
 locals {
   region = "ap-northeast-1"
-  instance_type = "t2.micro"
   system = "demo"
   env = "dev"
   myip = "0.0.0.0/0"
   instance_cnt = 1
   ami = "ami-072bfb8ae2c884cc4"
+  instance_type = "t2.micro"
 }

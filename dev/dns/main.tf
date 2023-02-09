919d6ca8f74f496f3a3554710cccb2935c839d8e
takahiro <takapi854@gmail.com>
2023-02-09 22:20:28 +0900

    update: DNS

---
+++ dev/dns/main.tf
@@ -0,0 +1,34 @@
+# resource "aws_route53_zone" "this" {
+#   name = local.hosted_zone
+
+# #   lifecycle {
+# #     prevent_destroy = true
+# #   }
+# }
+
+# resource "aws_route53_record" "this" {
+#   name = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_name
+#   type = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_type
+#   records = [
+#     tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_value
+#   ]
+
+#   zone_id = aws_route53_zone.this.zone_id
+#   ttl     = 60
+# }
+
+# resource "aws_acm_certificate" "this" {
+#   domain_name       = "*.${local.hosted_zone}"
+#   validation_method = "DNS"
+
+# #   lifecycle {
+# #     create_before_destroy = true
+# #   }
+# }
+
+# resource "aws_acm_certificate_validation" "this" {
+#   certificate_arn = aws_acm_certificate.this.arn
+#   validation_record_fqdns = [
+#     aws_route53_record.this.fqdn
+#   ]
+# }
---
+++ dev/dns/variables.tf
@@ -0,0 +1,7 @@
+locals {
+    region = "ap-northeast-1"
+    ip = "0.0.0.0/0"
+    hosted_zone = "tkoide.com"
+    env = "dev"
+    service = "demo"
+}

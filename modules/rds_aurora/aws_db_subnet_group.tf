831d781d9be5ea7b309bdd9487d877844cd40b2d
takahiro <takapi854@gmail.com>
2023-02-18 13:14:51 +0900

    update

--- modules/rds_aurora/aws_db_subnet_group.tf
+++ modules/rds_aurora/aws_db_subnet_group.tf
@@ -1,7 +1,44 @@
-# data "aws_db_subnet_group" "this" {
-#   name       = "my-test-database-subnet-group"
-  
-#   subnet_ids = [aws_subnet.subnet_1a.id, aws_subnet.subnet_1c.id]
 
-  
-# }
+
+####################################################
+# RDS SG
+####################################################
+
+resource "aws_security_group" "database_sg" {
+  name        = "${local.app_name}-database-sg"
+  description = "${local.app_name}-database"
+
+  vpc_id = aws_vpc.this.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${local.app_name}-database-sg"
+  }
+}
+
+resource "aws_security_group_rule" "database_sg_rule" {
+  security_group_id = aws_security_group.database_sg.id
+
+  type = "ingress"
+
+  from_port   = 3306
+  to_port     = 3306
+  protocol    = "tcp"
+  source_security_group_id = aws_security_group.app.id
+}
+
+resource "aws_db_subnet_group" "database_sg_group" {
+  name        = "${local.app_name}-database-subnet-group"
+  description = "${local.app_name}-database-subnet-group"
+  subnet_ids  = [
+    aws_subnet.private_1a.id,
+    aws_subnet.private_1c.id,
+    aws_subnet.private_1d.id,
+  ]
+}
--- modules/rds_aurora/variables.tf
+++ modules/rds_aurora/variables.tf
@@ -1,2 +1,10 @@
 variable "env" {}  
 variable "service" {}  
+
+locals {
+  app_name = "zenn"
+  host_domain = "<取得したドメイン>"
+  app_domain_name = "zenn-app.<取得したドメイン>"
+  api_domain_name = "zenn-api.<取得したドメイン>"
+  ssm_parameter_store_base = "/zenn/prod"
+}
